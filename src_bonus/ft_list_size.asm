b553d0391495ac2a7911066255590d4c2387f6be
aautin <alexandreautin.aa@gmail.com>
2025-05-29 16:38:51 +0200

    implement ft_list_size, not tested but compiling

--- src_bonus/ft_list_size.asm
+++ src_bonus/ft_list_size.asm
@@ -5,3 +5,40 @@
 ; }	t_list;
 
 ; int	ft_list_size(t_list* list)
+
+LIST_DATA equ 0
+LIST_NEXT equ 8
+
+section .text
+global ft_list_size
+
+ft_list_size:
+	push	rbp
+	mov		rbp, rsp
+
+	sub		rsp, 0x8
+	mov		qword [rbp - 0x8], rdi
+
+	xor		ecx, ecx
+
+	cmp		qword [rbp - 0x8], 0
+	je		.done
+
+	inc		ecx
+
+.loop:
+	mov		rdi, qword [rbp - 0x8]			; put -0x8 localvar value in rdi
+	mov		rdi, qword [rdi + LIST_NEXT]	; offset rdi to LIST_DATA
+	mov		qword [rbp - 0x8], rdi			; put next in -0x8 localvar
+
+	cmp		qword [rbp - 0x8], 0
+	je		.done
+
+	inc		ecx
+	jmp		.loop
+
+.done:
+	movsx		rax, ecx
+
+	leave
+	ret
