7aa2e443cfd8a8afde4b51e31112e767fca955da
aautin.home <alexandreautin.aa@gmail.com>
2025-05-30 03:51:37 +0200

    ft_list_sort 1st implementation, tested => crashing

--- src_bonus/ft_list_sort.asm
+++ src_bonus/ft_list_sort.asm
@@ -4,4 +4,93 @@
 ; 	struct s_list	*next;
 ; }	t_list;
 
+LIST_DATA equ 0x0
+LIST_NEXT equ 0x8
+
 ; void	ft_list_sort(t_list** list, int (*cmp)())
+
+extern ft_list_size
+
+section .text
+global ft_list_sort
+
+ft_list_sort:
+	push	rbp
+	mov		rbp, rsp
+
+	sub		rsp, 0x28					; (4 * 8) + (2 * 4) = 40 = 0x28
+	mov		qword [rbp - 0x8], rdi		; t_list**	start_ptr
+	mov		qword [rbp - 0x10], rsi		; int		(*cmp)()
+	mov		rdi, [rdi]
+	call	ft_list_size
+	mov		dword [rbp - 0x14], eax - 1	; int		sorting_loops_nb
+
+.sort_list:
+	dec		dword [rbp - 0x14]
+
+	cmp		dword [rbp - 0x14], 0
+	jle		.done
+
+	mov		dword [rbp - 0x18], 0		; set counter (i < sorting_loops_nb)
+	mov		qword [rbp - 0x20], 0		; set first ptr (previous left operand)
+	mov		rdi, qword [rbp - 0x8]
+	mov		rdi, qword [rdi] 
+	mov		qword [rbp - 0x28], rdi	; set second ptr (previous right operand)
+
+.sort_element:
+	mov		edi, dword [rbp - 0x18]
+	cmp		edi, dword [rbp - 0x14]
+	jge		.sort_list
+
+	mov		rdi, qword [rbp - 0x28]
+	mov		rdi, qword [rdi + LIST_DATA]	; set rdi on second ptr data
+
+	mov		rsi, qword [rbp - 0x28]
+	mov		rsi, qword [rsi + LIST_NEXT]
+	mov		rsi, qword [rsi + LIST_DATA]	; set rsi on second ptr next data
+
+	call	qword [rbp - 0x10]
+
+	cmp		eax, 0
+	jg		.swap
+
+	; mov the 2 pointers forward
+	mov		rsi, qword [rbp - 0x28]
+	mov		rdi, qword [rbp - 0x20]
+	mov		rdi, qword [rdi + LIST_NEXT]
+	mov		rsi, qword [rsi + LIST_NEXT]
+
+.sort_done:
+	inc		dword [rbp - 0x18]
+	jmp		.sort_element
+
+.swap:
+	; swap on the 2 first elements case
+	cmp		qword [rbp - 0x20], 0
+	cmove	rsi, qword [rbp - 0x28]
+	cmove	rsi, qword [rsi + LIST_NEXT]
+	cmove	rdi, qword [rbp - 0x8]
+	mov		qword [rdi], rsi
+
+	; save right operand next
+	mov		rdx, qword [rbp - 0x28]
+	mov		rdx, qword [rdx + LIST_NEXT]
+	mov		rdx, qword [rdx + LIST_NEXT]
+
+	; make right operand next point on left operand
+	mov		rcx, qword [rbp - 0x28 + LIST_NEXT]
+	cmp		rcx, 0
+	cmovne		r12, qword [rbp - 0x28]
+	cmovne		qword [rcx + LIST_NEXT], r12
+
+	; make left operand next point on the saved right operand next
+	mov		r13, qword [rbp - 0x28]
+	mov		qword [r13 + LIST_NEXT], rdx
+
+	; mov the 2 pointers forward
+	mov		qword [rbp - 0x20], r12
+	jmp		.sort_done
+
+.done:
+	leave
+	ret
--- src_bonus/main.c
+++ src_bonus/main.c
@@ -11,9 +11,9 @@ typedef struct		s_list
 // -------- BONUS FUNCS --------
 void	ft_list_push_front(t_list** begin_list, void* data);
 int 	ft_list_size(t_list *list);
+void 	ft_list_sort(t_list **begin_list, int (*cmp)());
 
 /*
-void 	ft_list_sort(t_list **begin_list, int (*cmp)());
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, 
 		int (*cmp)(), void (*free_fct)(void *));
 int		ft_atoi_base(char *str, char *base);
@@ -51,6 +51,14 @@ int	main(int argc, char** argv)
 	}
 
 	printf("list size: %d\n", ft_list_size(list));
+
+	t_list* ptr = list;
+	for (int i = 0; ptr != NULL; ++i) {
+		printf("[%d] %s\n", i, (char*)ptr->data);
+		ptr = ptr->next;
+	}
+
+	ft_list_sort(&list, (int (*)(void*, void*))strcmp);
 
 	for (int i = 0; list != NULL; ++i) {
 		printf("[%d] %s\n", i, (char*)list->data);
