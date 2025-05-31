42581c4ad62436fb02cb4c4115dc47b8d13180df
aautin.home <alexandreautin.aa@gmail.com>
2025-05-31 15:35:19 +0200

    ft_atoi_base handle isspaces and minus sign

--- src_bonus/ft_atoi_base.asm
+++ src_bonus/ft_atoi_base.asm
@@ -1,8 +1,8 @@
 ; int	ft_atoi_base(char* str, int base_length)
 
 STRING		equ 0x8
-BASE_LEN	equ 0x10
-SIGN		equ 0x14
+BASE_LEN	equ 0xc
+MINUS		equ 0x10
 BASE		equ 0x18
 RES			equ 0x20
 
@@ -19,12 +19,12 @@ ft_atoi_base:
 	; stack managment
 	push	rbp
 	mov		rbp, rsp
-	sub		rsp, 0x28
+	sub		rsp, 0x20
 
 	; local variables initialisation
 	mov		qword [rbp - STRING], rdi		; STRING = str
 	mov		dword [rbp - BASE_LEN], esi		; BASE_LEN = base_length
-	mov		byte [rbp - SIGN], 1			; SIGN = 1
+	mov		byte [rbp - MINUS], 0			; MINUS = 0
 	lea     rax, [rel base]
 	mov		qword [rbp - BASE], rax			; BASE = base
 	mov		dword [rbp - RES], 0			; RES = 0
@@ -35,13 +35,50 @@ ft_atoi_base:
 	cmp		dword [rbp - BASE_LEN], 16
 	jg		.end
 
+.spaces_loop:
+	; if *STRING == 0
+	mov		rdi, qword [rbp - STRING]
+	cmp		byte [rdi], 0
+	je		.end
+
+	; if *STRING == ' ' : isspace
+	cmp		rdi, qword [rbp - STRING]
+	cmp		byte [rdi], 0x20
+	je		.isspace
+
+	; if *STRING < '\t' : not isspace
+	cmp		rdi, qword [rbp - STRING]
+	cmp		byte [rdi], 0x9
+	jl		.minus
+
+	; if *STRING > '\r' : not isspace
+	cmp		rdi, qword [rbp - STRING]
+	cmp		byte [rdi], 0xd
+	jg		.minus
+
+	jmp		.isspace
+
+.isspace:
+	inc		qword [rbp - STRING]
+	jmp		.spaces_loop
+
+.minus:
+	; if *STRING != '-'
+	mov		rdi, qword [rbp - STRING]
+	cmp		byte [rdi], 0x2d
+	jne		.loop
+
+	; MINUS = 1 and STR++
+	mov		byte [rbp - MINUS], 1
+	inc		qword [rbp - STRING]
+
 .loop:
 	; while *str != 0
 	mov		rdi, qword [rbp - STRING]
 	cmp		byte [rdi], 0
 	je		.end
 
-	; get_index(BASE, BASE_LEN, *STR)
+	; str_index(BASE, BASE_LEN, *STR)
 	mov		rdi, qword [rbp - BASE]
 	mov		esi, dword [rbp - BASE_LEN]
 	mov		rdx, qword [rbp - STRING]
@@ -66,7 +103,15 @@ ft_atoi_base:
 	jmp		.loop
 
 .end:
-	; set return value and restore stack
+	; set return value
 	mov		eax, dword [rbp - RES]
+
+	; optionally apply minus
+	cmp		byte [rbp - MINUS], 1
+	jne		.return
+	imul    eax, eax, -1
+
+.return:
+	; restore stack
 	leave
 	ret
